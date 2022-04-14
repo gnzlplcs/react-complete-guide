bddf558424b196e86c7fac9a86b9f1c129cdd2d0
gnzlplcs <palaciosgm@outlook.com>
2022-04-14 14:45:37 -0500

    :recycle: changing hard code for dynamic data (lesson 36)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 2, 28);
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const expenseTitle = "Car Inssurance";
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{`${days[expenseDate.getDay()]}, ${expenseDate.getDate()}th ${months[expenseDate.getMonth()]} ${expenseDate.getFullYear()}`}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
