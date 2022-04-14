8f4d3109054015a9a7f247e9fa1df223fa0a4fc0
gnzlplcs <palaciosgm@outlook.com>
2022-04-14 15:22:43 -0500

    :zap: passing props from App to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 28);
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  const expenseTitle = "Car Inssurance";
-  const expenseAmount = 294.67;
-
+const ExpenseItem = ({title, amount, date}) => {
   return (
     <div className="expense-item">
-      <div>{`${days[expenseDate.getDay()]}, ${expenseDate.getDate()}th ${months[expenseDate.getMonth()]} ${expenseDate.getFullYear()}`}</div>
+      <div>{date}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
