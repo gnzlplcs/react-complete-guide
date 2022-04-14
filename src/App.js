c9d12e933b8318f80f0e4532d17ef7956fbedee7
gnzlplcs <palaciosgm@outlook.com>
2022-04-14 15:33:10 -0500

    :sparkles: implement uuid
    add uuid npm package to set random ids for each ExpenseItem component instace

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import ExpenseItem from "./components/ExpenseItem";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const expenses = [
     {
-      id: "e1",
       title: "Toiler Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
     {
-      id: "e2",
       title: "New TV",
       amount: 799.49,
       date: new Date(2021, 2, 12),
     },
     {
-      id: "e3",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: "e4",
       title: "New Desk (wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
@@ -36,7 +33,7 @@ function App() {
           title={expense.title}
           amount={expense.amount}
           date={expense.date.toISOString()}
-          key={expense.id}
+          key={uuidv4()}
         />
       ))}
     </div>
