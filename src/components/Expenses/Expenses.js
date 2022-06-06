fa4f35a051384b4e230a8dcee8afb2c72e52f29f
gnzlplcs <palaciosgm@outlook.com>
2022-06-05 23:39:08 -0500

    💪🏻code works + I had to compare my code with Max's code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = ({ items }) => {
@@ -16,13 +17,16 @@ const Expenses = ({ items }) => {
   });
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        onFilterYear={selectedYearHandler}
-        selected={filteredYear}
-      />
-      <ExpensesList items={filteredByYear}/>
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onFilterYear={selectedYearHandler}
+        />
+        <ExpensesChart expenses={filteredByYear} />
+        <ExpensesList items={filteredByYear}/>
+      </Card>
+    </div>
   );
 };
 
